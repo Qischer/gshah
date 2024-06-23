eebf26dfd876d6850f2ebd1a753ff188cdfcd85e
Mark Nguy <56264886+Qischer@users.noreply.github.com>
2024-06-23 14:15:33 -0400

    Update movegen.go

--- game/movegen.go
+++ game/movegen.go
@@ -8,7 +8,7 @@ func generate(piece board.DisplayPiece, pos uint64) uint64 {
 
   switch piece {
     case board.BKing:
-      generateKing(pos)
+      mask = generateKing(pos)
       break
     default:
       break
@@ -19,7 +19,9 @@ func generate(piece board.DisplayPiece, pos uint64) uint64 {
 
 func generateKing(pos uint64) uint64 {
 
-  mask := (pos << 7) | (pos << 8) | (pos << 9) | (pos >> 1) | (pos << 1) | (pos >> 9) | (pos >> 8) | (pos >> 7)
+  var mask uint64
+
+  mask = (pos << 7) | (pos << 8) | (pos << 9) | (pos >> 1) | (pos << 1) | (pos >> 9) | (pos >> 8) | (pos >> 7)
 
   return mask
 }
